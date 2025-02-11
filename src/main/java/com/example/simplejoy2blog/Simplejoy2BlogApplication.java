d83541ba0d1f5e5c130e4e39580ee8bdc72c5bdc
VerySimple99 <lyricrain.seo@gmail.com>
2025-02-11 18:50:04 +0900

    update:  gradle add: properties

---
+++ src/main/java/com/example/simplejoy2blog/Simplejoy2BlogApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplejoy2blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Simplejoy2BlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Simplejoy2BlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/simplejoy2blog/Simplejoy2BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplejoy2blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Simplejoy2BlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
