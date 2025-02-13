5459af134beb49a11a4b77bf5548db710ab2f8c4
VerySimple99 <lyricrain.seo@gmail.com>
2025-02-13 18:18:52 +0900

    update: findAll

--- src/main/java/com/example/simplejoy2blog/domain/Article.java
+++ src/main/java/com/example/simplejoy2blog/domain/Article.java
@@ -3,10 +3,12 @@ package com.example.simplejoy2blog.domain;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 public class Article{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/example/simplejoy2blog/service/BlogService.java
+++ src/main/java/com/example/simplejoy2blog/service/BlogService.java
@@ -6,13 +6,17 @@ import com.example.simplejoy2blog.repository.BlogRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @Service
 public class BlogService {
     private final BlogRepository blogRepository;
 
     public Article save(AddArticleRequest request) {
-
         return blogRepository.save(request.toEntity());
+    }
+    public List<Article> findAll(){
+        return blogRepository.findAll();
     }
 }
